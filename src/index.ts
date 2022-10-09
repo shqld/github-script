387d7345edbf7bd6515c2770d0200eb8c6e6a8bd
Sho Miyamoto <me@shqld.dev>
2022-10-09 22:47:46 +0900

    fix imports from @actions/*

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,18 @@
 require('sucrase/register')
 require('zx/globals')
 
-import { context, getOctokit } from '@actions/github'
-import * as core from '@actions/core'
-import * as glob from '@actions/glob'
-import * as io from '@actions/io'
-import * as exec from '@actions/exec'
+// import type { context, getOctokit } from '@actions/github'
+// import type * as core from '@actions/core'
+// import type * as glob from '@actions/glob'
+// import type * as io from '@actions/io'
+// import type * as exec from '@actions/exec'
+
+const { context, getOctokit } =
+    require('@actions/github') as typeof import('@actions/github')
+const core = require('@actions/core') as typeof import('@actions/core')
+const glob = require('@actions/glob') as typeof import('@actions/glob')
+const io = require('@actions/io') as typeof import('@actions/io')
+const exec = require('@actions/exec') as typeof import('@actions/exec')
 
 export interface Arg {
     github: ReturnType<typeof getOctokit>
