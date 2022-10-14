87431b6c9f7baf2d3e0a39e925deab791cb211df
Sho Miyamoto <me@shqld.dev>
2022-10-14 17:30:52 +0900

    fix module

--- src/ci.ts
+++ src/ci.ts
@@ -3,21 +3,3 @@
 
 require('sucrase/register')
 require('zx/globals')
-
-const __original_module_require__ = require
-
-function __hooked_module_require__(moduleName: string) {
-    if (moduleName === '@shqld/github-script') {
-        return { main }
-    }
-
-    __original_module_require__(moduleName)
-}
-
-Object.assign(__hooked_module_require__, require)
-
-module.require = __hooked_module_require__ as typeof require
-
-function main(func: Function) {
-    return (args: unknown) => func(args)
-}
