39f1b94166645c38318072e8d84119e7e449a7a9
Sho Miyamoto <me@shqld.dev>
2022-10-14 16:54:00 +0900

    fix hook

--- src/ci.ts
+++ src/ci.ts
@@ -4,13 +4,19 @@
 require('sucrase/register')
 require('zx/globals')
 
-require('require-in-the-middle')(
-    ['@shqld/github-script'],
-    { internals: true },
-    (_exports, _name, _basedir) => {
+const __original_module_require__ = require
+
+function __hooked_module_require__(moduleName: string) {
+    if (moduleName === '@shqld/github-script') {
         return { main }
     }
-)
+
+    __original_module_require__(moduleName)
+}
+
+Object.assign(__hooked_module_require__, require)
+
+module.require = __hooked_module_require__ as typeof require
 
 function main(func: Function) {
     return (args: unknown) => func(args)
